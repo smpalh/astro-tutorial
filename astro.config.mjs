6e44b854f3b5290c725894cae4506a8e5539967e
smpalh <box.shiitake@gmail.com>
2023-09-22 05:33:20 +0900

    プリフェッチを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [prefetch({
+    throttle: 3,
+  })]
+});
