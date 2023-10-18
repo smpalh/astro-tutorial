615650aea48a1bb8a6c0e81d9ad8e78fd8b1ccd2
smpalh <box.shiitake@gmail.com>
2023-10-18 12:40:26 +0900

    add MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import prefetch from "@astrojs/prefetch";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [prefetch({
-    throttle: 3,
-  })]
+    throttle: 3
+  }), mdx()]
 });
