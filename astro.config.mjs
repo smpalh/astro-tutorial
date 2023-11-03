57a39f0e59cf423ca66c1eafdc25120cd8f74ba0
smpalh <box.shiitake@gmail.com>
2023-11-04 00:23:07 +0900

    preactを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import prefetch from "@astrojs/prefetch";
-
 import mdx from "@astrojs/mdx";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [prefetch({
     throttle: 3
-  }), mdx()]
+  }), mdx(), preact()]
 });
